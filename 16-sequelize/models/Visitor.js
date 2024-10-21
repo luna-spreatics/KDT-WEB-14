97c764507b74399bde68bd0d010634bb297eef25
luna-spreatics <luna@spreatics.com>
2024-10-21 13:56:29 +0900

    sequelize models

---
+++ 16-sequelize/models/Visitor.js
@@ -0,0 +1,7 @@
+// @param {import('sequelize').Sequelize} Sequelize : sequelize 라이브러리에서 Sequelize 클래스를 가져와서 명시
+/**
+ * visitor 모델을 정의하는 함수
+ * @param {import('sequelize').Sequelize} Sequelize
+ * @param {import('sequelize').DataTypes} DataTypes
+ */
+const Visitor = (Sequelize, DataTypes) => {};
