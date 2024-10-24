edebe4c84c1670334efade65305ddaa3876acf65
luna-spreatics <luna@spreatics.com>
2024-10-24 14:01:29 +0900

    env

---
+++ 18-env/index.js
@@ -0,0 +1,12 @@
+// const ps = process.env;
+// console.log(ps);
+
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
