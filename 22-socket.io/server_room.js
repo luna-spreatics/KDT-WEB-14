bb6bc36851282a424b318de952bbf62ea840abe5
luna-spreatics <luna@spreatics.com>
2024-11-16 14:01:10 +0900

    socket.io study

--- 22-socket.io/server_room.js
+++ 22-socket.io/server_room.js
@@ -9,21 +9,64 @@ const PORT = 8000;
 
 // 사용자 닉네임 모음 객체
 const nickObjs = {}; // {socket.id: nick1, socket.id: nick2..}
+// 채팅방 정보 저장 배열
+const roomList = [];
+
+// 채팅방에 참여한 유저 조회
+function getUsersInRoom(room) {
+  // 사용자 정보를 저장하는 배열
+  const users = [];
+
+  // 지정된 방에 클라이언트(socket id) 목록 가져오기
+  const clients = io.sockets.adapter.rooms.get(room);
+  console.log(`${room} 채팅방의 클라이언트 목록 조회 :: ${clients}`);
+
+  if (clients) {
+    for (const socketId of clients) {
+      // 특정 소켓 ID에 해당하는 클라이언트 소켓 정보 가져오기
+      const socket = io.sockets.sockets.get(socketId);
+      const nickname = socket.user || "알수없음";
+
+      const info = { nickname, key: socketId };
+      users.push(info);
+    }
+  }
+
+  return users;
+}
 
 app.set("view engine", "ejs");
 app.use("/static", express.static(__dirname + "/static"));
 
 app.get("/", (req, res) => {
-  res.render("chat");
+  res.render("room");
 });
 
 io.on("connection", (socket) => {
   console.log("서버 연결 완료 :: ", socket.id);
 
-  // [실습1]
-  socket.on("hello", (data, callback) => {
-    console.log(`${data.who} : ${data.msg}`);
-    socket.emit("helloKr", { who: "hello", msg: "안녕!" });
+  // 클라이언트 연결 시 채팅방 목록 표시
+  io.emit("updateRoom", roomList);
+
+  // 채팅방 만들기
+  socket.on("create", ({ roomName, nickname }, cb) => {
+    // join(방이름): 해당 방이름이 없다면 생성, 있다면 입장
+    // socket.rooms 에 socket.id 값과 방 이름 확인 가능
+    socket.join(roomName);
+
+    // 채팅방에 입장한 사용자에게 notice 이벤트 발생
+    socket.to(roomName).emit("notice", `${nickname} 님이 입장하였습니다`);
+
+    // socket 정보에 닉네임 추가
+    socket.user = nickname;
+
+    // 채팅방 목록 갱신
+    if (!roomList.includes(roomName)) {
+      roomList.push(roomName);
+      io.emit("updateRoom", roomList);
+    }
+    getUsersInRoom(roomName);
+    cb();
   });
 
   // [실습3] 채팅창 입장 문구
