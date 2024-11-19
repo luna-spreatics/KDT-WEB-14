8a4365e9654d86630157b04aaaeb8a42587ff0bf
luna-spreatics <luna@spreatics.com>
2024-11-19 13:50:53 +0900

    study react state

---
+++ 25-state/src/App.js
@@ -0,0 +1,16 @@
+import Counter from "./Counter";
+import SayFunction from "./SayFunction";
+
+function App() {
+  return (
+    <div className="App">
+      {/* 클래스형 state */}
+      <Counter />
+      <hr />
+
+      <SayFunction />
+    </div>
+  );
+}
+
+export default App;
---
+++ 25-state/src/SayFunction.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function SayFunction() {
+  console.log(useState("welcome!")); // ['welcome!', f]
+  const [message, setMessage] = useState("welcome!");
+  // 'welcome!' : 상태 초기값, (숫자, 문자, 배열 값의 형태 자유로움)
+  // message : 메세지 상태
+  // setMessage() : message state 값을 바꾸는 함수
+
+  const onClickEnter = () => {
+    setMessage("안녕하세요~");
+  };
+
+  const onClickLeave = () => {
+    setMessage("안녕히 가세요~");
+  };
+
+  return (
+    <div>
+      <button onClick={onClickEnter}>입장</button>
+      <button onClick={onClickLeave}>퇴장</button>
+      <h1>{message}</h1>
+    </div>
+  );
+}
