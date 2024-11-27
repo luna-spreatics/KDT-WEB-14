05945f91d8a3edd6595d27ed39f2cb54a908287d
luna-spreatics <luna@spreatics.com>
2024-11-27 14:01:32 +0900

    study redux

---
+++ 33-redux/src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+import "./index.css";
+import App from "./App";
+import rootReducer from "./store";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// store 정의 : 전역 상태를 관리하는 하나의 공간 (하나의 프로젝트에 하나만!)
+const store = configureStore({ reducer: rootReducer });
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
